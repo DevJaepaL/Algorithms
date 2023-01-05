e5ed6343a88f48fd095f9cc3a838c851ad374adf
DevJaepaL <wocks3254@gmail.com>
2023-01-05 18:52:40 +0900

    :: Resolved ✔️ ::
    :heavy_plus_sign:  주석 추가.

--- Programmers/src/Programmers_Lv0/Lv0_ControlZ.java
+++ Programmers/src/Programmers_Lv0/Lv0_ControlZ.java
@@ -1,27 +1,30 @@
 package Programmers_Lv0;
 import java.util.Stack;
-
+/*
+ * Programmers Lv.0 : 컨트롤 제트
+ * Site : https://school.programmers.co.kr/learn/courses/30/lessons/120853
+ * 해결 방식  : Stack 자료구조 이용
+ */
 public class Lv0_ControlZ {
     public int solution(String s) {
         int answer = 0;
         String[] divideStr = s.split("\\s");
         Stack<Integer> stack = new Stack<>();
 
-        while(stack.size() != 0)
-        {
-            for(int i = 0; i < divideStr.length; i++) {
-                    if(!divideStr[i].equals("Z")) {
-                        stack.push(Integer.parseInt(divideStr[i]));
-                    }
-
-                    else {
-                        if(stack.size() >= 1)
-                            stack.pop();
-                    }
+        
+        for(int i = 0; i < divideStr.length; i++) {
+            if(!divideStr[i].equals("Z")) {
+                stack.push(Integer.parseInt(divideStr[i]));
             }
 
-            return answer += stack.pop();
-            
+            else
+                if(stack.size() >= 1)
+                    stack.pop();
         }
+
+        while(stack.size() != 0)
+            answer += stack.pop();
+
+        return answer;
     }
 }
