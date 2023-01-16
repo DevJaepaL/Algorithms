22f178765e8e2b36046c01d2ee8e1c36b4cfe4e2
DevJaepaL <wocks3254@gmail.com>
2023-01-16 17:20:30 +0900

    :: Resolved ✔️ ::
    문제 2914 [저작권] 해결 !

---
+++ BaekJoon/src/PythonSeries1/Solution_2914.java
@@ -0,0 +1,20 @@
+/*
+ *  백준 문제 2914 : 저작권
+ *  Site : https://www.acmicpc.net/problem/2914
+ */
+import java.util.*;
+public class Solution_2914 {
+    static void calc(int a , int b) {
+        int result = a * b;
+        result -= a;
+        System.out.println(result + 1);
+    }
+   public static void main(String args[])  {
+        Scanner sc = new Scanner(System.in);
+        int albumSongs = sc.nextInt();
+        int avg = sc.nextInt();
+        sc.close();
+    
+        calc(albumSongs, avg);
+   }
+}
