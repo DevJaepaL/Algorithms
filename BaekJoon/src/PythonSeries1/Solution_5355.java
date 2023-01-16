ec19634b86f0689d9cafc94434c4747358646495
DevJaepaL <wocks3254@gmail.com>
2023-01-16 18:26:38 +0900

    :: Resolved ✔️ ::
    * 백준 5355 : [화성 수학] *
    문제 해결 : ✔️
    피드백 : 핵심은, Float.parseFloat이랑,
    각 인덱스 번호 순회마다 case문으로 특정 문자값 받을 시 계산 해주는 것!

---
+++ BaekJoon/src/PythonSeries1/Solution_5355.java
@@ -0,0 +1,41 @@
+/*
+ *  백준 문제 5355 : 화성 수학
+ *  Site : https://www.acmicpc.net/problem/5355
+ */
+import java.util.*;
+public class Solution_5355 {
+    static void marsCalc(String str) {
+        String[] arr = str.split(" ");
+            float calcNum = Float.parseFloat(arr[0]); 
+
+            for(int j = 1; j < arr.length; j++) 
+            {
+                switch(arr[j])
+                {
+                    case "@": calcNum *= 3;
+                        break;
+                    case "%": calcNum += 5;
+                        break;
+                    case "#": calcNum -= 7;
+                        break;
+                    default:
+                        continue;
+                }
+            }
+            System.out.println(String.format("%.2f", calcNum));
+    }
+
+   public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        for(int i = 0; i < n; i++)
+        {
+            String input = sc.nextLine();
+            marsCalc(input);
+        }
+
+        sc.close();
+   }
+}
