37d9f7874bd8055da8ee8b0ee6b05944b8c31352
DevJaepaL <wocks3254@gmail.com>
2023-01-18 19:45:03 +0900

    :: 문제 [2935]: 소음 :white_check_mark: ::
    * 백준 2935 : [소음] *
    문제 해결 : ✔️
    피드백 : BigInteger 활용

---
+++ BaekJoon/src/PythonSeries1/Solution_2935.java
@@ -0,0 +1,22 @@
+package PythonSeries1;
+/*
+ *  백준 문제 2935 : 소음
+ *  Site : https://www.acmicpc.net/problem/2935
+ */
+import java.math.BigInteger;
+import java.util.*;
+public class Solution_2935 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger num0 = new BigInteger(sc.nextLine());
+        String calc = sc.nextLine();
+        BigInteger num1 = new BigInteger(sc.nextLine());
+
+        if(calc.equals("*"))
+            System.out.println(num0.multiply(num1));
+        else
+            System.out.println(num0.add(num1));
+
+        sc.close();
+    }
+}
