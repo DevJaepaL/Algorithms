a4aadb7463704a597661640bab4c8fa817a8fa20
DevJaepaL <wocks3254@gmail.com>
2023-01-20 21:42:12 +0900

    :: 문제 [4101]: 크냐? :white_check_mark: ::
    * 백준 4101 : [크냐?] *
    문제 해결 : ✔️

---
+++ BaekJoon/src/PythonSeries1/Solution_4101.java
@@ -0,0 +1,15 @@
+package PythonSeries1;
+import java.util.*;
+public class Solution_4101 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        while(true)
+        {
+            int num0 = sc.nextInt();
+            int num1 = sc.nextInt();
+            if(num0 == 0 && num1 == 0) break;
+            String print = num0 > num1 ?  "Yes" : "No";
+            System.out.println(print);
+        }
+    }
+}
