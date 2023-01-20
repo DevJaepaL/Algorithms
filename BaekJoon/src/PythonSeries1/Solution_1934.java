f9ea4fff2de17fda2d924e90fea205eade5e0f0d
DevJaepaL <wocks3254@gmail.com>
2023-01-20 21:30:45 +0900

    :: 문제 [1934]: 최소공배수 :white_check_mark: ::
    * 백준 10039 : [평균 점수] *
    문제 해결 : ✔️
    피드백 : 문제 설명
    https://velog.io/@jaepal/Java-BOJ-1934-%EC%B5%9C%EC%86%8C%EA%B3%B5%EB%B0%B0%EC%88%98

--- BaekJoon/src/PythonSeries1/Solution_1934.java
+++ BaekJoon/src/PythonSeries1/Solution_1934.java
@@ -10,11 +10,12 @@ public class Solution_1934 {
         String[] numArr = str.split(" ");
         int lcdNum0 = Integer.parseInt(numArr[0]);
         int lcdNum1 = Integer.parseInt(numArr[1]);
-        int maxNum = (lcdNum0 > lcdNum1) ? lcdNum0 : lcdNum1; // 최소 값 구하기
+        int maxNum = (lcdNum0 > lcdNum1) ? lcdNum0 : lcdNum1; // 최대 값 구하기
         int lcm = 0;
         for(int i = 1; i <= maxNum; i++) {
                 if(lcdNum0 % i == 0 && lcdNum1 % i == 0) {
                         int gcd = i; // 두 수의 최대공배수
+                        System.out.println(gcd);
                         lcm = lcdNum0 * lcdNum1 / gcd;
                     }
                 }
