85fbd5199824db47ee2ced48cef6474c0740ace9
JaepaL <104006620+DevJaepaL@users.noreply.github.com>
2023-01-21 02:23:15 +0900

    :: 문제 [3009]: 네 번째 점 :white_check_mark: ::
    * 백준 3009 : [네 번째 점] *
    문제 해결 : ✔️
    피드백 : 난이도 높아질수록 메모리 제한  이랑 제한 시간이 걸리는 걸 감안해서 BufferedReader랑 StringTokenizer 라이브러리 이용하려고 함. Scanner만 쭉 사용하다보니 적응하기 어려운데, 연습합시다!

---
+++ BaekJoon/src/PythonSeries1/Solution_3009.java
@@ -0,0 +1,30 @@
+package PythonSeries1;
+/*
+ *  백준 문제 3009 : 네 번째 점
+ *  Site : https://www.acmicpc.net/problem/3009
+ */
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+public class Solution_3009 {
+    public static void main(String args[]) throws IOException {
+        int x,y;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        int[] point_0 = { Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+        st = new StringTokenizer(br.readLine(), " ");
+        int[] point_1 = { Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+        st = new StringTokenizer(br.readLine(), " ");
+        int[] point_2 = { Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+
+        if(point_0[0] == point_1[0])        x = point_2[0];
+        else if(point_0[0] == point_2[0])   x = point_1[0];
+        else                                x = point_0[0];
+        if(point_0[1] == point_1[1])        y = point_2[1];
+        else if(point_0[1] == point_2[1])   y = point_1[1];
+        else                                y = point_0[1];
+        System.out.print(x + " " + y);
+    }
+}
