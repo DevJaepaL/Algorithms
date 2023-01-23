bea0f2316df566a1ed24a54f69c8636bc66dc344
JaepaL <104006620+DevJaepaL@users.noreply.github.com>
2023-01-23 17:24:17 +0900

    :: 문제 [10988]: 팰린드롬인지 확인하기 :white_check_mark: ::
    * 백준 10988 : [팰린드롬인지 확인하기] *
    문제 해결 : ✔️
    피드백 : 팰린드롬 알고리즘을 알아봐야하는 문제였다.
    1. 입력받은 문자열의 길이 / 2 (중간값)만큼 루프를 돌린다.
    -> 그 이유는, 팰린드롬은 데칼코마니 처럼 중간값을 기준으로 양 옆의 값이 같기 때문!
    2. 조건으로 첫 번째 문자와 마지막 문자(문자열 길이 - 1 - 루프 최초 값)가 같은지 체크 후 같지 않으면 False를 리턴한다. 이 외의 값은 팰린드롬인 것을 확인할 수 있다.

---
+++ BaekJoon/src/PythonSeries1/Solution_10988.java
@@ -0,0 +1,23 @@
+package PythonSeries1;
+/*
+ *  백준 문제 10988 : 팰린드롬인지 확인하기
+ *  Site : https://www.acmicpc.net/problem/10988
+ */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+public class Solution_10988 {
+    static void palindrome(String str) {
+        int check = 1;
+        for(int i = 0; i < str.length() / 2; i++) {
+            if(str.charAt(i) != str.charAt(str.length() - i - 1))
+                check = 0;
+        }
+            System.out.println(check);
+    }
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String palind = br.readLine();
+        palindrome(palind);
+    }
+}
