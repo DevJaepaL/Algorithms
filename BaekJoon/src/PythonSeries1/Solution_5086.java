29eb028c089b91bee46df5afc461c393c9ea23d3
JaepaL <104006620+DevJaepaL@users.noreply.github.com>
2023-01-23 17:25:44 +0900

    :: 문제 [5086]: 배수와 약수 :white_check_mark: ::
    * 백준 5086 : [배수와 약수] *
    문제 해결 : ✔️

---
+++ BaekJoon/src/PythonSeries1/Solution_5086.java
@@ -0,0 +1,36 @@
+package PythonSeries1;
+/*
+ *  백준 문제 5086 : 배수와 약수
+ *  Site : https://www.acmicpc.net/problem/5086
+ */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+public class Solution_5086 {
+    static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true)
+        {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            if(n == 0 && m == 0) 
+                break;
+            else if(n < m && m % n == 0) {
+                System.out.println("factor");
+            }
+            else if(n > m && n % m == 0) {
+                System.out.println("multiple");
+            }
+            else {
+                System.out.println("neither");
+            }
+        }
+        
+        br.close();
+    }
+    public static void main(String args[]) throws Exception {
+        solution();
+    }
+}
