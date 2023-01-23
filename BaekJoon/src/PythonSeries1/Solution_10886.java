d4f54bb3dedd65be9181a39f7d69163b881f9398
JaepaL <104006620+DevJaepaL@users.noreply.github.com>
2023-01-23 17:21:27 +0900

    :: 문제 [10886]: 0 = not cute / 1 = cute :white_check_mark: ::
    * 백준 10886 : [0 = not cute / 1 = cute] *
    문제 해결 : ✔️

---
+++ BaekJoon/src/PythonSeries1/Solution_10886.java
@@ -0,0 +1,29 @@
+package PythonSeries1;
+/*
+ *  백준 문제 100886 : 0 = not cute / 1 = cute
+ *  Site : https://www.acmicpc.net/problem/10886
+ */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+public class Solution_10886 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = Integer.parseInt(br.readLine());
+        int cute = 0;
+        int notCute = 0;
+
+        for(int i = 0; i < cnt; i++) {
+            int cuteVote = Integer.parseInt(br.readLine());
+            if(cuteVote == 1) 
+                cute += 1;
+            else 
+                notCute += 1;
+        }
+
+        if(cute < notCute)
+            System.out.println("Junhee is not cute!");
+        else
+            System.out.println("Junhee is cute!");
+    }
+}
