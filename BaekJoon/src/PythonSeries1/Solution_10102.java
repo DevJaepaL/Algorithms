da70178e16f4965d2b420ab2967814ec9b9acffa
JaepaL <104006620+DevJaepaL@users.noreply.github.com>
2023-01-23 17:20:56 +0900

    :: 문제 [10102]: 개표 :white_check_mark: ::
    * 백준 10102 : [개표] *
    문제 해결 : ✔️

---
+++ BaekJoon/src/PythonSeries1/Solution_10102.java
@@ -0,0 +1,30 @@
+package PythonSeries1;
+/*
+ *  백준 문제 10102 : 개표
+ *  Site : https://www.acmicpc.net/problem/10102
+ */
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+public class Solution_10102 {
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int picks = Integer.parseInt(br.readLine());
+        String vote = br.readLine();
+        int aCnt = 0;
+        int bCnt = 0;
+        for(int i = 0; i < vote.length(); i++) {
+            if(vote.charAt(i) == 'A') 
+                aCnt += 1;
+            else 
+                bCnt += 1;
+        }
+
+        if(aCnt < bCnt)
+            System.out.println("B");
+        else if(aCnt == bCnt) 
+            System.out.println("Tie");
+        else 
+            System.out.println("A");
+    }
+}
