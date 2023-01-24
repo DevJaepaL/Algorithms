004653e311bfc4125133184140924042db535165
JaepaL <104006620+DevJaepaL@users.noreply.github.com>
2023-01-24 14:04:35 +0900

    :: 문제 [10162]: 전자레인지 :white_check_mark: ::
    * 백준 10162 : [전자레인지] *
    문제 해결 : ✔️

---
+++ BaekJoon/src/PythonSeries1/Solution_10162.java
@@ -0,0 +1,28 @@
+package PythonSeries1;
+/*
+ *  백준 문제 10162 : 전자레인지
+ *  Site : https://www.acmicpc.net/problem/10162
+ */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class Solution_10162 {
+    public static void main(String args[]) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int timer = Integer.parseInt(br.readLine());
+        int A = 0;
+        int B = 0;
+        int C = 0;
+        if (timer % 10 != 0)
+            System.out.print(-1);
+        else {
+            A = timer / 300;
+            timer = timer % 300;
+            B = timer / 60;
+            timer = timer % 60;
+            C = timer / 10;
+            System.out.printf("%d %d %d", A,B,C);
+        }
+
+        br.close();
+    }
+}
