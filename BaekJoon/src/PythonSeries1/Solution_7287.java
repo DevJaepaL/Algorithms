9a1f19e61c55297f04df20dc47bbf953bb46683a
DevJaepaL <wocks3254@gmail.com>
2023-01-27 20:58:21 +0900

    :: 문제 [7287] : 등록 :white_check_mark: ::
    * 백준 7287 : [등록] *
    문제 해결 : ✔️

---
+++ BaekJoon/src/PythonSeries1/Solution_7287.java
@@ -0,0 +1,20 @@
+package PythonSeries1;
+/*
+ *  백준 문제 7287 : 등록
+ *  Site : https://www.acmicpc.net/problem/7287
+ */
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+public class Solution_7287 {
+    static void Solution() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("81 \n");
+        bw.write("wocks3254");
+        bw.close();
+    }
+    
+    public static void main(String args[]) throws IOException {
+        Solution();
+    }
+}
