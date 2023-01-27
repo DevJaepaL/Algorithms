ca5a4f32445dd0c3a7e5205e9188243441b30f5a
DevJaepaL <wocks3254@gmail.com>
2023-01-27 21:45:33 +0900

    :: 문제 [1977] : 완전제곱수 :white_check_mark: ::
    문제 No. 1977 : 완전제곱수
    문제 해결 : ✔️
    피드백 : 브루트 포스 알고리즘의 의미를 알았다.
    해당 문제에서 최대 범위(10,000)를 지정해줬기에 100까지의 완전제곱수를
    배열에 다 넣고 입력 값(N , M)의 범위에 있는 완전제곱수만 찾도록 코드를 작성해봤다.

---
+++ BaekJoon/src/PythonSeries2/Solution_1977.java
@@ -0,0 +1,39 @@
+package PythonSeries2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+public class Solution_1977 {
+    static void findSquare(int A, int B) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int minNum = 10000; // 10,000 is 100 square;
+        int sumSqr = 0;
+        int[] sqr = new int[101];
+
+        for(int i = 1; i < sqr.length; i++) {
+            sqr[i] = i * i;
+            if(sqr[i] >= A && sqr[i] <= B) {
+                sumSqr += sqr[i];
+                if(sqr[i] < minNum)
+                    minNum = sqr[i];
+            }    
+        }    
+
+        if(sumSqr == 0)
+            bw.write("-1");
+        else {
+            bw.write(String.valueOf(sumSqr) + "\n" + minNum);
+        }
+        bw.close();
+    }
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        br.close();
+        findSquare(N, M);
+    }
+}
