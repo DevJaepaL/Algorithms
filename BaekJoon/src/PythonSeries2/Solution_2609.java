a3987df9e15947b440831af5de8891465fafe38d
JaepaL <104006620+DevJaepaL@users.noreply.github.com>
2023-02-02 14:18:57 +0900

    add<Solution_2609.java> : Solution_2609 문제 해결 ✅

--- BaekJoon/src/PythonSeries2/Solution_2609.java
+++ BaekJoon/src/PythonSeries2/Solution_2609.java
@@ -1,2 +1,29 @@
-package PythonSeries2;public class Solution_2609 {
+package PythonSeries2;
+
+import java.io.*;
+import java.util.StringTokenizer;
+public class Solution_2609 {
+    static long GCD(long A, long B) {
+        long gcd = 0;
+        long max = Math.max(A, B);
+        for(int i = 1; i <= max; i++) {
+            if(A % i == 0 && B % i == 0)
+                gcd = i;
+        }
+        return gcd;
+    }
+
+    static void LCM(long A, long B) {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        System.out.println(GCD(A , B));
+        System.out.print(A * B / GCD(A,B));
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Integer.parseInt(st.nextToken());
+        long b = Integer.parseInt(st.nextToken());
+        LCM(a, b);
+        br.close();
+    }
 }
