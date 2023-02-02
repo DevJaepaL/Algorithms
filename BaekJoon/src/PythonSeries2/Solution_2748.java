a1ac9893f7dbc9247fc4374285b478b545dee165
JaepaL <104006620+DevJaepaL@users.noreply.github.com>
2023-02-03 01:47:06 +0900

    add<Solution_2748.java> : 문제 해결 ✅
    처음으로 DP를 접해본 문제.
    이 문제를 재귀호출을 이용하면 컴퓨터 박살날 듯

--- BaekJoon/src/PythonSeries2/Solution_2748.java
+++ BaekJoon/src/PythonSeries2/Solution_2748.java
@@ -1,2 +1,27 @@
-package PythonSeries2;public class Solution_2748 {
+package PythonSeries2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+public class Solution_2748 {
+    public static long[] DP = new long[91];
+    public static long fibonacci(int A) {
+        if(A == 1 || A == 2)
+            return 1;
+        if(DP[A] != 0)
+            return DP[A];
+        DP[A] = fibonacci(A - 1)  + fibonacci(A - 2);
+        return DP[A];
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        bw.write(String.valueOf(fibonacci(N)));
+        br.close();
+        bw.close();
+    }
 }
