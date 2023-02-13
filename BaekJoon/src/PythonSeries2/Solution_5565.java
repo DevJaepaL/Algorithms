b5c32d8f44cfbcfa72700c505962cdb02afa0c89
JaepaL <104006620+DevJaepaL@users.noreply.github.com>
2023-02-13 19:16:35 +0900

    Add Files<Solution_5565.java> : 문제 5565 해결 ✅

---
+++ BaekJoon/src/PythonSeries2/Solution_5565.java
@@ -0,0 +1,23 @@
+package PythonSeries2;
+
+import java.io.*;
+
+public class Solution_5565 {
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void solution(int B) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int booksPrice = 0;
+        for(int i = 1; i < 10; i++) {
+            int readingBooks = Integer.parseInt(br.readLine());
+            booksPrice += readingBooks;
+        }
+        B -= booksPrice;
+        bw.write(String.valueOf(B));
+        bw.close();
+        br.close();
+    }
+    public static void main(String[] args) throws IOException {
+        int b = Integer.parseInt(br.readLine());
+        solution(b);
+    }
+}
