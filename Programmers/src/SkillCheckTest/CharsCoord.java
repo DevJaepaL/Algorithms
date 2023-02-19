f5ce686af4359c3c1e6b591169f7692fa0049c01
DevJaepaL <wocks3254@gmail.com>
2023-02-20 00:48:40 +0900

    Edit<CharsCoord.java> : 소스 코드 수정

--- Programmers/src/SkillCheckTest/CharsCoord.java
+++ Programmers/src/SkillCheckTest/CharsCoord.java
@@ -1,51 +1,32 @@
 package SkillCheckTest;
 
 public class CharsCoord {
+  public int[] solution(String[] keyinput, int[] board) {
+    int xCnt = 0;
+    int yCnt = 0;
+    final int leftCoord = -board[0] / 2;
+    final int rightCoord = board[0] / 2;
+    final int downCoord = -board[1] / 2;
+    final int upCoord = board[1] / 2;
 
-    public int[] solution(String[] keyinput, int[] board) {
-        int xCnt = 0;
-        int yCnt = 0;
-        final int leftCoord = -board[0] / 2;
-        final int rightCoord = board[0] / 2;
-        final int downCoord = -board[1] / 2;
-        final int upCoord = board[1] / 2;
-
-        for(int i = 0; i < keyinput.length; i++) {
-            switch(keyinput[i])
-            {
-              case "down":
-                yCnt -= 1;
-                break;
-              case "up":
-                yCnt += 1;
-                break;
-              case "left":
-                xCnt -= 1;
-                break;
-              case "right":
-                xCnt += 1;
-                break;
-            }    
-        }
-        
-        if(xCnt >= leftCoord) {
-            xCnt = leftCoord;
-        } else if(xCnt >= rightCoord) {
-            yCnt = rightCoord;
-        }
-
-        if(yCnt >= downCoord) {
-            yCnt = downCoord;
-        } else if(yCnt >= upCoord) {
-            yCnt = upCoord;
-        }
-
-        int[] answer = {xCnt, yCnt};
-        return answer;
+    for (int i = 0; i < keyinput.length; i++) {
+      switch (keyinput[i]) {
+        case "down":
+          if (yCnt > downCoord) yCnt -= 1;
+          break;
+        case "up":
+          if (yCnt < upCoord) yCnt += 1;
+          break;
+        case "left":
+          if (xCnt > leftCoord) xCnt -= 1;
+          break;
+        case "right":
+          if (xCnt < rightCoord) xCnt += 1;
+          break;
+      }
     }
 
-    public static void main(String[] args) {
-        int[] arr = {7,9};
-        System.out.println(-arr[0] / 2);
-    }
+    int[] answer = { xCnt, yCnt };
+    return answer;
+  }
 }
