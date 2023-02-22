9f25f8ce3d65fc2d65f31c715d99f86ec6b315cb
DevJaepaL <wocks3254@gmail.com>
2023-02-22 21:51:07 +0900

    Resolved ☑️

--- Programmers/src/Programmers_Lv0/Lv0_EvenNum.java
+++ Programmers/src/Programmers_Lv0/Lv0_EvenNum.java
@@ -4,7 +4,7 @@ public class Lv0_EvenNum {
     public int solution(int n) {
         int answer = 0;
         for(int i = 1; i <= n; i++) {
-            if(n % i == 0) {
+            if(i % 2 == 0) {
                 answer += i;
             }
         }
---
+++ Programmers/src/Programmers_Lv0/Lv0_PrintAge.java
@@ -0,0 +1,8 @@
+package Programmers_Lv0;
+
+public class Lv0_PrintAge {
+    public int solution(int age) {
+        int answer = (2022 - age) + 1;
+        return answer;
+    } 
+}
