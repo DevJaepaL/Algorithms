b934ed345cf19eb1cffcc16182ef178af269ff0b
DevJaepaL <wocks3254@gmail.com>
2023-03-01 22:02:27 +0900

    RESOLVED<Programmers> : 문제 "삼각형의 완성조건(1)" 해결 ✅

---
+++ Programmers/src/Programmers_Lv0/Lv0_Triangle.java
@@ -0,0 +1,13 @@
+package Programmers_Lv0;
+
+import java.util.Arrays;
+
+class Lv0_Triangle {
+    public int solution(int[] sides) {
+        int answer = 0;
+        Arrays.sort(sides);
+        int maxVal = sides[2];
+        int remainVal = sides[0 + 1];
+        return answer;
+    }
+}
