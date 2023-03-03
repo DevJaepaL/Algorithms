633c7bdd2a6d4c13b8ef746e715e184513e13194
DevJaepaL <wocks3254@gmail.com>
2023-03-04 02:17:47 +0900

    RESOLVING<BaekJoon> : 문제 "수얼과 쿼리 38" 풀이 중 🔧

---
+++ BaekJoon/src/Silver/Solution_18917.java
@@ -0,0 +1,56 @@
+package Silver;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Solution_18917 {
+    public static void solution() {
+        Scanner sc = new Scanner(System.in);
+        int QUERY = sc.nextInt();
+        for(int i = 1; i < QUERY; i++) 
+        {
+            int sumVal = 0;
+            int xorVal = 0;
+            ArrayList<Integer> queryArr = new ArrayList<>(QUERY);
+            queryArr.add(0, 0);
+            int querySelect = sc.nextInt();
+            int xValue = querySelect <= 2 ? sc.nextInt() : 0;
+            
+            switch(querySelect)
+            {
+                case 1:
+                    queryArr.add(xValue);
+                    break;
+
+                case 2:
+                    for(int findNum : queryArr) {
+                        if(queryArr.contains(findNum)) {
+                            queryArr.remove(findNum);
+                            break;
+                        }
+                    }
+                    break;
+
+                case 3:
+                    for(int index : queryArr) {
+                        index = queryArr.get(index);
+                        sumVal += index;
+                    }
+                    System.out.println(sumVal);
+                    break;
+                    
+                case 4:
+                    for(int index = 1; i < queryArr.size(); index++) {
+                        xorVal += queryArr.get(0) ^ queryArr.get(index);
+                    }
+                    System.out.println(xorVal);
+                    break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        solution();
+    }
+}
