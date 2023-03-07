e001c563b182c9eddc97afa09b319250e6d3df0a
JaepaL <104006620+DevJaepaL@users.noreply.github.com>
2023-03-07 22:47:20 +0900

    <RESOLVED> : 문제 "피자 나눠 먹기(1)" 해결 ☑️

--- Programmers/src/Programmers_Lv0/Lv0_SlicePizza1.java
+++ Programmers/src/Programmers_Lv0/Lv0_SlicePizza1.java
@@ -1,2 +1,10 @@
-package Programmers_Lv0;public class Lv0_SlicePizza1 {
+package Programmers_Lv0;
+
+public class Lv0_SlicePizza1 {
+    public int solution(int n) {
+        /*  파라미터(사람 수)값이 7로 나눠떨어지지 않으면 나눈 값에 + 1 해준다.
+         *  나눠 떨어진다면(7의 배수) 7로 나눈 값 그대로 리턴
+         */
+        return  n % 7 != 0 ? n / 7 + 1 : n / 7;
+    }
 }
