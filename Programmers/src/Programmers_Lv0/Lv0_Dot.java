0d052bcc1ddfafc4adf7b10414807d5f53833993
DevJaepaL <wocks3254@gmail.com>
2023-03-27 21:43:05 +0900

    <RESOLVED> : 문제 "점의 위치 구하기" 해결 🔼

---
+++ Programmers/src/Programmers_Lv0/Lv0_Dot.java
@@ -0,0 +1,17 @@
+package Programmers_Lv0;
+
+class Lv0_Dot {
+    public int solution(int[] dot) {
+        int x = dot[0];
+        int y = dot[1];
+        if (x > 0 && y > 0) {
+            return 1;
+        } else if (x < 0 && y >= 0) {
+            return 2;
+        } else if (x < 0 && y < 0) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+}
