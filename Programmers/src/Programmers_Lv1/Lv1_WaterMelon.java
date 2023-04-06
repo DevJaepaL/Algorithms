0e0176617f2298264778192d45ae3b32819f406a
JaepaL <104006620+DevJaepaL@users.noreply.github.com>
2023-04-06 22:08:03 +0900

    <Programmers_Lv1> - "수박" 해결 ✅

---
+++ Programmers/src/Programmers_Lv1/Lv1_WaterMelon.java
@@ -0,0 +1,15 @@
+package Programmers_Lv1;
+
+public class Lv1_WaterMelon {
+    public String solution(int n) {
+        String answer = "";
+        for(int i = 0; i < n; i++) {
+            if(i % 2 == 0) {
+                answer += "수";
+            } else {
+                answer += "박";
+            }
+        }
+        return answer;
+    }
+}
